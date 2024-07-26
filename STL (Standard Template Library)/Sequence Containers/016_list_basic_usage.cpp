74258d49d512299e006cd9e75c2831985633be80
helyousfi <visioncomputer200@gmail.com>
2024-07-26 22:50:29 +0200

    List added in containers

---
+++ STL (Standard Template Library)/Sequence Containers/016_list_basic_usage.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <list>
+
+int main()
+{
+    std::list<int> myList = {1, 5, 6, 8, 3, 1};
+    std::cout << "Printing my list";
+    for(int n : myList)
+    {
+        std::cout << n << ' ';
+    }
+    std::cout << std::endl;
+
+    myList.push_back(6); // Add to the end
+    myList.push_front(0); // Add to the beginning
+
+    std::cout << "After adding elements: ";
+    for (int n : myList) {
+        std::cout << n << ' ';
+    }
+
+    std::cout << std::endl;
+
+    myList.pop_back(); // Remove from the end
+    myList.pop_front(); // Remove from the beginning
+
+    return 0;
+}
