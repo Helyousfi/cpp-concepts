4ea4be8ef360af5d3ac845ce70f64ec0c82a10c4
helyousfi <visioncomputer200@gmail.com>
2024-07-29 18:52:34 +0200

    Casting added

---
+++ Casting in C++/casting_cpp.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+class Base {
+public:
+	virtual void Show() { std::cout << "Base Class!" << std::endl; }
+};
+class Derived : public Base {
+public:	
+	void Show() override { std::cout << "Derived Class!" << std::endl; }
+};
+
+int main()
+{
+	// Implicit casting 
+	int i = 10;
+	double di = i;
+	std::cout << "Implicit casting : " << di << std::endl;
+	
+	// C-style casting
+	double pi = 3.14;
+	int intPi = (int)pi;
+	std::cout << "C-style cast : " << intPi << std::endl;
+
+	// Static cast
+	int a = 65;
+	double d = static_cast<double>(a);
+	std::cout << "Static cast : " << d << std::endl;
+
+	// Dynamic cast
+	Base* base = new Base();
+	Derived* derived = dynamic_cast<Derived*>(base);
+	if(derived) 
+		derived->Show();
+
+	// Reinterpret cast
+	int n = 1;
+	char* c = reinterpret_cast<char*>(&n);
+	std::cout << "Reinterpret cast : " << static_cast<int>(*c) << std::endl;
+
+	return 0;
+}
+
+
