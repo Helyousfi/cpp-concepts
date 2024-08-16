cf00abe822055e38ddcd145fbf4f401c4f3060b2
BilalELOTMANI <bilalelotmani100@gmail.com>
2024-08-16 18:44:28 +0200

    Initial commit

---
+++ Pointers & References/Pointers/09_function_pointers.cpp
@@ -0,0 +1,22 @@
+// 09_function_pointers.cpp
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    int (*operation)(int, int); // Function pointer
+
+    operation = &add;
+    std::cout << "Add: " << operation(10, 5) << std::endl;
+
+    operation = &subtract;
+    std::cout << "Subtract: " << operation(10, 5) << std::endl;
+
+    return 0;
+}
