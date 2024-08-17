d57da25f3d7c805c043530d76cc3016ba3964d3c
BilalELOTMANI <bilalelotmani100@gmail.com>
2024-08-17 04:27:28 +0200

    Initial commit

---
+++ Pointers & References/References/02_references_and_functions.cpp
@@ -0,0 +1,15 @@
+// 02_references_and_functions.cpp
+#include <iostream>
+
+void increment(int& ref) {
+    ref++; // Modifies the original variable
+}
+
+int main() {
+    int a = 10;
+    increment(a);
+
+    std::cout << "Value of a after increment: " << a << std::endl;
+
+    return 0;
+}
