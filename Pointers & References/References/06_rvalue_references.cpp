af4a566b60d9f230e14fc40dfee81059c240a5ad
BilalELOTMANI <bilalelotmani100@gmail.com>
2024-08-17 04:31:26 +0200

    Initial commit

---
+++ Pointers & References/References/06_rvalue_references.cpp
@@ -0,0 +1,14 @@
+// 06_rvalue_references.cpp
+#include <iostream>
+
+void printValue(int&& ref) { // rvalue reference
+    std::cout << "Rvalue reference: " << ref << std::endl;
+}
+
+int main() {
+    int a = 10;
+    printValue(10); // OK: 10 is an rvalue
+    // printValue(a); // Error: a is an lvalue
+
+    return 0;
+}
