5795babaa8acff0ed83766884ccf83ebe4e61460
RADARIST <visioncomputer200@gmail.com>
2024-10-07 15:31:26 +0200

    Update cpp_basics.cpp

--- 01-Basics/cpp_basics.cpp
+++ 01-Basics/cpp_basics.cpp
@@ -1,66 +1,58 @@
-/*
- * - Variables and Data Types
- * - Basic Input and Output
- * - Control Structures (if-else, loops)
- * - Functions
- * - Arrays
- */
+// cpp_basic_syntax.cpp
+// Basic Syntax in C++
 
-#include <iostream>  // Required for input/output operations
-using namespace std; // Allows the use of standard names without prefixing them with "std::"
-
-
-// Function to demonstrate the use of functions
-int add(int a, int b) {
-    return a + b; // Returns the sum of a and b
-}
+#include <iostream> // Required for cin, cout
+using namespace std;
 
 int main() {
-    // Variable declaration
-    int x = 10;      // Integer variable
-    float y = 20.5;  // Floating-point variable
-    char z = 'A';    // Character variable
-    bool isTrue = true; // Boolean variable
 
-    // Printing the values of variables
-    cout << "Integer: " << x << endl;
-    cout << "Float: " << y << endl;
-    cout << "Character: " << z << endl;
-    cout << "Boolean: " << isTrue << endl;
+    // Comments in C++
+    // Single-line comment
 
-    // Taking user input
-    int a, b;
-    cout << "Enter two integers: ";
-    cin >> a >> b; // Storing user input in variables a and b
+    /* 
+       Multi-line comment
+    */
 
-    // Using the function
-    int sum = add(a, b);
-    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+    // Data Types
+    int integer = 10;             // Integer data type
+    float decimal = 3.14f;        // Float data type
+    char character = 'A';         // Character data type
+    bool is_true = true;          // Boolean data type (true or false)
 
-    // Conditional statement
-    if (sum > 10) {
-        cout << "The sum is greater than 10." << endl;
-    } else {
-        cout << "The sum is 10 or less." << endl;
-    }
+    // Variables and Constants
+    int num = 20;                 // Variable (value can be changed)
+    const float PI = 3.14159;     // Constant (value cannot be changed)
 
-    // Looping statement
-    cout << "Counting from 1 to 5:" << endl;
-    for (int i = 1; i <= 5; i++) {
-        cout << i << " "; // Outputs numbers from 1 to 5
-    }
-    cout << endl;
+    // Input/Output
+    cout << "Enter a number: ";
+    int userInput;
+    cin >> userInput;             // Taking input from the user
+    cout << "You entered: " << userInput << endl; // Outputting the result
 
-    // Array declaration
-    int arr[5] = {1, 2, 3, 4, 5};
+    // Operators
+    // Arithmetic Operators: +, -, *, /, %
+    int a = 10, b = 3;
+    cout << "a + b = " << a + b << endl;   // Addition
+    cout << "a - b = " << a - b << endl;   // Subtraction
+    cout << "a * b = " << a * b << endl;   // Multiplication
+    cout << "a / b = " << a / b << endl;   // Division
+    cout << "a % b = " << a % b << endl;   // Modulus
 
-    // Accessing array elements
-    cout << "Array elements: ";
-    for (int i = 0; i < 5; i++) {
-        cout << arr[i] << " "; // Outputs array elements
-    }
-    cout << endl;
+    // Logical Operators: && (AND), || (OR), ! (NOT)
+    bool condition = (a > b) && (a != 0);  // Logical AND
+    cout << "Logical AND (a > b && a != 0): " << condition << endl;
 
-    return 0; // Indicates that the program ended successfully
+    condition = (a < b) || (a == 10);      // Logical OR
+    cout << "Logical OR (a < b || a == 10): " << condition << endl;
+
+    condition = !(a == 10);                // Logical NOT
+    cout << "Logical NOT !(a == 10): " << condition << endl;
+
+    // Relational Operators: ==, !=, >, <, >=, <=
+    cout << "Is a equal to b? " << (a == b) << endl;   // Equality
+    cout << "Is a not equal to b? " << (a != b) << endl; // Inequality
+    cout << "Is a greater than b? " << (a > b) << endl;  // Greater than
+    cout << "Is a less than or equal to b? " << (a <= b) << endl; // Less than or equal to
+
+    return 0;
 }
-
